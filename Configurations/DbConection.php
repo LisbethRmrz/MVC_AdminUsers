b0827b3726d07c01767a4c0576097909cee29745
LisbethRmrz <lisbethrmrz98@gmail.com>
2022-03-17 10:30:38 -0600

    Agregando archivo readme

--- Configurations/DbConection.php
+++ Configurations/DbConection.php
@@ -5,6 +5,7 @@
 		public static function conection(){
 			
 			$conn = new mysqli("localhost", "lisbeth", "123456", "admin_users");
+			//$conn = new mysqli("sql310.epizy.com", "epiz_31312474", "kyr96UpAxJl3UoM", "epiz_31312474_admin_users");
 			return $conn;
 			
 		}
---
+++ Readme.md
@@ -0,0 +1,5 @@
+# MVC Usuarios
+
+Este mini proyecto consiste en la gestión de usuarios utilizando PHP y el modelo vista controlador
+
+## Requisitos
